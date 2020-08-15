0cc0e3e61800d260d6ea9e838dafdde5f307f46e
Amit <akp.332@gmail.com>
2020-08-15 23:31:24 +0530

    Updates

---
+++ precache-manifest.2771c59b3069dda4da11b7a06b627061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f31d544a476c9a261478e44ee594ca",
+    "url": "/git-hub-pages-mytest/index.html"
+  },
+  {
+    "revision": "9864e421b37f564c1337",
+    "url": "/git-hub-pages-mytest/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "7f2839d10ae82b0904ce",
+    "url": "/git-hub-pages-mytest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9864e421b37f564c1337",
+    "url": "/git-hub-pages-mytest/static/js/2.ac9271e1.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/git-hub-pages-mytest/static/js/2.ac9271e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2839d10ae82b0904ce",
+    "url": "/git-hub-pages-mytest/static/js/main.e7bf768b.chunk.js"
+  },
+  {
+    "revision": "99eba36ca4515fb4752c",
+    "url": "/git-hub-pages-mytest/static/js/runtime-main.17e2acb9.js"
+  }
+]);
